31a9300748b158ae5e0870007ffe3f06c0e6884c
Davey Wright <davey.w.wright@gmail.com>
2018-09-04 16:24:39 +0100

    feat add automatic remember me on log in

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
   devise_for :users, :controllers => {
     registrations: 'registrations',
+    sessions: 'sessions',
     omniauth_callbacks: 'omniauth_callbacks'}
   resources :users, only: :show
 end
