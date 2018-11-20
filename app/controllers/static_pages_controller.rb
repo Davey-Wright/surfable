ea38913c101b3e95ce70e5b90f383f2d60d4edc0
Davey Wright <davey.w.wright@gmail.com>
2018-11-20 10:28:41 +0000

    add form styles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,4 @@
 class StaticPagesController < ApplicationController
-  helper_method :resource_name, :resource, :devise_mapping, :resource_class
-
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def resource_class
-    User
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
+  def home
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
+
     private
+
       def sign_up_params
         params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
       end
