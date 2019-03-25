d66f1cc54df7e1a00b460f53173ee40f7785825b
Davey Wright <davey.w.wright@gmail.com>
2019-03-25 16:42:31 +0000

    add dynamic body class helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def body_css_class
+    @body_css_classes ||= []
+    view_css_class = [controller_path.split('/'), action_name].flatten.join('_')
+    @body_css_classes.unshift(view_css_class).join(' ')
+  end
+
   def tide_offsets_value(type, value)
     case value
     when 1
