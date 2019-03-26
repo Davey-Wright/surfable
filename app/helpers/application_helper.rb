1dfbcbed3c0747c495f3868f808d3e4178c04a5f
Davey Wright <davey.w.wright@gmail.com>
2019-03-26 11:43:57 +0000

    homepage layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  def body_css_class
-    @body_css_classes ||= []
-    view_css_class = [controller_path.split('/'), action_name].flatten.join('_')
-    @body_css_classes.unshift(view_css_class).join(' ')
+  def body_css_id
+    @body_css_id ||= []
+    view_css_id = [controller_path.split('/'), action_name].flatten.join('_')
+    @body_css_id.unshift(view_css_id).join(' ')
   end
 
   def tide_offsets_value(type, value)
